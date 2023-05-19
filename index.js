d750f326a5c37a49d583ad3b4a9d02085aa55e36
ADRIAN RINCON <107867643+AdrianYRincon@users.noreply.github.com>
2023-05-19 18:23:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,27 +9,8 @@ const app = express();
 // Base de datos
 dbConnection();
 
-
-const dominiosPermitidos = [process.env.FRONTEND_URL];
-
-const corsOptions = {
-
-  origin: function(origin, callback) {
-    if(dominiosPermitidos.indexOf(origin) !==-1){
-      //El origen del Request esta permitido
-      callback(null, true);
-    }
-    else{
-      callback(new Error('No permitido por CORS'))
-      
-    }
-  }
-
-};
-
-
 // CORS
-app.use(cors( corsOptions));
+app.use(cors());
 // Directorio Público
 app.use( express.static('public') );
 
